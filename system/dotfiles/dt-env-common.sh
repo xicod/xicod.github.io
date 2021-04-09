7e4d11540306cfae520e7f1938503adfd681251d
Denis Tokarev <denistok@xicod.com>
2021-04-09 16:58:19 +0300

    store the ssh socket files in /tmp instead of HOME

--- system/dotfiles/dt-env-common.sh
+++ system/dotfiles/dt-env-common.sh
@@ -219,7 +219,7 @@ _dt_term_socket_ssh() {
 	ssh -oControlPath=$1 -O exit DUMMY_HOST
 }
 function sshx {
-	local t=$(mktemp -u --tmpdir=$HOME ssh.sock.XXXXXXXXXX)
+	local t=$(mktemp -u --tmpdir ssh.sock.XXXXXXXXXX)
 	local f="~/clip"
 	ssh -f -oControlMaster=yes -oControlPath=$t $@ tail\ -f\ /dev/null || return 1
 	ssh -S$t DUMMY_HOST "bash -c 'if ! [ -p $f ]; then mkfifo $f; fi'" \
