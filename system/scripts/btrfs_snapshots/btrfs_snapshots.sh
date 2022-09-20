f8b012db68fea6cfc63073759a3f896a1f9103fc
Denis Tokarev <denistok@xicod.com>
2022-09-20 18:13:10 +0300

    btrfs_snapshots

---
+++ system/scripts/btrfs_snapshots/btrfs_snapshots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# cron sometimes sets HOME=/
+export HOME=$(getent passwd $(whoami) | cut -d':' -f6)
+
+source /etc/profile.d/dt-profile.sh
+source dt-app-conf.sh
+
+if [ $DTCONF_silent != "FALSE" ]; then
+	exec 1>>/dev/null
+fi
+
+ts=$(date +%Y%m%d_%H%M%S)
+
+IFS=, read -ra targets <<< "$DTCONF_targets"
+for target in "${targets[@]}"; do
+	IFS=: read -r \
+		days_to_keep \
+		target_path \
+	<<< "$target"
+
+	snapshot_base=${target_path}/snapshots/@daily_
+
+	btrfs_clean_snapshots.sh $((60*60*24*${days_to_keep} - 60)) ${snapshot_base}'*'
+	btrfs subvolume snapshot -r ${target_path}/@master ${snapshot_base}${ts}
+
+	echo
+done
