38fb428c559e354c007e3cd052e321d05865a3d8
Denis Tokarev <denistok@xicod.com>
2024-10-01 04:24:42 -0700

    require a space

--- system/tools/restic_html_colorize.sh
+++ system/tools/restic_html_colorize.sh
@@ -3,8 +3,8 @@
 echo '<pre>'
 
 sed \
-	-e 's|^\(+\s*/.*\)$|<span style="color: green;">\1</span>|' \
-	-e 's|^\(-\s*/.*\)$|<span style="color: red;">\1</span>|' \
-	-e 's|^\(M\s*/.*\)$|<span style="color: blue;">\1</span>|'
+	-e 's|^\(+\s\+/.*\)$|<span style="color: green;">\1</span>|' \
+	-e 's|^\(-\s\+/.*\)$|<span style="color: red;">\1</span>|' \
+	-e 's|^\(M\s\+/.*\)$|<span style="color: blue;">\1</span>|'
 
 echo '</pre>'
