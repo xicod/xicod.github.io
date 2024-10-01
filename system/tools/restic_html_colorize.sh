d88ade1b02ee6cf94f213efa802c3464ce19c6ba
Denis Tokarev <denistok@xicod.com>
2024-10-01 04:19:59 -0700

    better just use pre

--- system/tools/restic_html_colorize.sh
+++ system/tools/restic_html_colorize.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo '<pre>'
+
 sed \
 	-e 's|^\(+\s*/.*\)$|<span style="color: green;">\1</span>|' \
 	-e 's|^\(-\s*/.*\)$|<span style="color: red;">\1</span>|' \
-	-e 's|^\(M\s*/.*\)$|<span style="color: blue;">\1</span>|' \
-	-e 's|$|<br>|'
+	-e 's|^\(M\s*/.*\)$|<span style="color: blue;">\1</span>|'
+
+echo '</pre>'
