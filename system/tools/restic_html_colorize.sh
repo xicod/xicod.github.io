16688c97d4e324c00372d7705c4ad62f150f93ce
Denis Tokarev <denistok@xicod.com>
2024-10-03 12:51:18 -0700

    take pre out to a separate script

--- system/tools/restic_html_colorize.sh
+++ system/tools/restic_html_colorize.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-echo '<pre>'
-
 sed \
 	-e 's|^\(+\s\+/.*\)$|<span style="color: green;">\1</span>|' \
 	-e 's|^\(-\s\+/.*\)$|<span style="color: red;">\1</span>|' \
 	-e 's|^\(M\s\+/.*\)$|<span style="color: blue;">\1</span>|'
-
-echo '</pre>'
---
+++ system/tools/wrap_with_pre.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo '<pre>'
+cat
+echo '</pre>'
